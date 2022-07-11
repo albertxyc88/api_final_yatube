9d42ad448d0305efbda61d1dcc103b02c910d3d1
Альберт Хуснадинов <albertxyc@gmail.com>
2022-07-11 23:18:41 +0300

    For review. Ver.3. Fix permissions.

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -49,10 +49,9 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post', )
